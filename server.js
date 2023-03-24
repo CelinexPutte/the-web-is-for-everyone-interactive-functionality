5cfeca267af977fc5e795829e794f7537be84690
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-03-24 13:29:33 +0100

    School dagje
    - Custom scrollbar
    - extra form velden
    - lay-out form aangepast

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //import express en dotenv 
-import express from 'express'
+import express from "express"
 
 //maak een nieuwe express app
 const server = express()
 
 //public map gebruiken
-server.use(express.static('public'))
+server.use(express.static("public"))
 
 //stel de views in
-server.set('view engine', 'ejs')
-server.set('views', './views')
+server.set("view engine", "ejs")
+server.set("views", "./views")
 
 // Stel afhandeling van formulieren in
 server.use(express.json())
@@ -19,37 +19,37 @@ server.use(express.urlencoded({ extended: true }))
 const urlAPI = "https://api.codingthecurbs.fdnd.nl/api/v1"
 // const data = await fetch(url).then((response) => response.json())
 
-server.get('/', (request, response) => {
+server.get("/", (request, response) => {
     let urlSmartzones = `${urlAPI}/smartzones`
   fetchJson(urlSmartzones).then((smartzones) => {
-    let id = request.query.id || 'clene4gw60aqg0bunwwpawr1p'
+    let id = request.query.id || "clene4gw60aqg0bunwwpawr1p"
     let url = `${urlAPI}/reservations?id=${id}`
     fetchJson(url).then((reservations) => {
       let data = {smartzones: smartzones, reservations: reservations} 
-      response.render('index', data)
+      response.render("index", data)
     })
   })
 })
 
 
-server.get('/book', (request, response) => {
+server.get("/book", (request, response) => {
   let urlSmartzones = `${urlAPI}/smartzones`
   fetchJson(urlSmartzones).then((smartzones) => {
-    let id = request.query.id || 'clene4gw60aqg0bunwwpawr1p'
+    let id = request.query.id || "clene4gw60aqg0bunwwpawr1p"
     let url = `${urlAPI}/reservations?id=${id}`
     fetchJson(url).then((reservations) => {
       let data = {smartzones: smartzones, reservations: reservations}
-      response.render('book', data)
+      response.render("book", data)
     })
   })
 })
 
 //poortnummer instellen
-server.set('port', 8000)
+server.set("port", 8000)
 
 //start de server
-server.listen(server.get('port'), () => {
-  console.log(`Application started on http://localhost:${server.get('port')}`)
+server.listen(server.get("port"), () => {
+  console.log(`Application started on http://localhost:${server.get("port")}`)
 })
 
 /**
@@ -73,9 +73,9 @@ async function fetchJson(url) {
  */
 export async function postJson(url, body) {
   return await fetch(url, {
-    method: 'post',
+    method: "post",
     body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.json())
     .catch((error) => error)
