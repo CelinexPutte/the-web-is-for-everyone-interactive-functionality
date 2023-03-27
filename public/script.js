af149606fc458a28d6a0b689ce47bb8d58e66eea
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-03-27 13:16:14 +0200

    Automatisch huidige datum inladen

---
+++ public/script.js
@@ -0,0 +1,26 @@
+// Huidige datum automatisch inladen
+const dateInput = document.getElementById('date-from');
+const dateOutput = document.getElementById("date-till");
+// ✅ Using the visitor's timezone
+dateInput.value = formatDate();
+dateOutput.value = formatDate();
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date = new Date()) {
+  return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+  ].join('-');
+}
+// ✅ Using UTC (universal coordinated time)
+dateInput.value = new Date().toISOString().split('T')[0];
+
+
+// Automatisch einddatum meeveranderen
+dateInput.onchange = () => {
+    dateOutput.value = dateInput.value
+}
