d7076301390706548ddb58cd94baf12a980bcc00
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-03-27 12:52:13 +0200

    Opzet post
    Api kapot dus niet kunnen testen

--- server.js
+++ server.js
@@ -48,6 +48,35 @@ server.get("/book", (request, response) => {
   })
 })
 
+server.post('/book', (request, response) => {
+  console.log(request.body)
+  const url = urlAPI + "/reservations"
+  request.body.timeStart = request.body.dateStart + 'T' + request.body.timeStart + ':00Z';
+  request.body.timeEnd = request.body.dateEnd + 'T' + request.body.timeEnd + ':00Z';
+
+  postJson(url, request.body).then((data) => {
+    let newReservation = { ... request.body }
+    console.log(JSON.stringify(data), 'hoiiii')
+
+    if (data.success) {
+      response.redirect('/?reservationPosted=true') 
+    } else {
+      const errorMessage = data.message + "Some fields are not filled in (correctly)."
+      const newData = { error: errorMessage, values: newReservation }
+      
+      let urlSmartzones = urlAPI + '/smartzones'
+        fetchJson(urlSmartzones).then((smartzones) => {
+          let id = request.query.id || "clene4gw60aqg0bunwwpawr1p"
+          let url = urlAPI + '/reservations?id=' + id
+            fetchJson(url).then((reservations) => {
+              let data = {smartzones: smartzones, reservations: reservations, newData}
+      response.render("book", data)
+            })
+        })
+    }
+  })
+})
+
 //poortnummer instellen
 server.set("port", 8000)
 
